65071c8597ba7e3ea9a088879a2628ef3c2688c4
shubhjain08 <65031443+shubhjain08@users.noreply.github.com>
2020-05-29 13:59:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,90 @@
+//global variables
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var box1, box2, box3;
+
+//creating a physics engine
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+function preload()
+{
+    //loading images
+    helicopterIMG=loadImage("helicopter.png")
+    packageIMG=loadImage("package.png")
+}
+
+function setup() {
+    //canvas
+    createCanvas(800, 700);
+    
+    rectMode(CENTER);
+    
+
+    packageSprite=createSprite(width/2, 80, 10,10);
+    packageSprite.addImage(packageIMG)
+    packageSprite.scale=0.2
+
+    helicopterSprite=createSprite(width/2, 200, 10,10);
+    helicopterSprite.addImage(helicopterIMG)
+    helicopterSprite.scale=0.6
+
+    groundSprite=createSprite(width/2, height-35, width,10);
+    groundSprite.shapeColor=color(255)  
+
+
+    engine = Engine.create();
+    world = engine.world;
+
+    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.62, isStatic:true});
+    World.add(world, packageBody);
+    
+
+    //Create a Ground
+    ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+     World.add(world, ground);
+    
+     boxPosition=width/2-100
+     boxY=610;
+     
+     box1= new Box(boxPosition+100, boxY+40, 200,20);
+     box2= new Box(boxPosition,boxY,20,100);
+     box3= new Box(boxPosition+200,boxY,20,100);
+     
+    
+     boxOne= Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20,{isStatic:true})
+     World.add(world,boxOne);
+     boxTwo= Bodies.rectangle(+20,boxY,20,100,{isStatic:true})
+     World.add(world,boxTwo);
+     boxThree= Bodies.rectangle(boxPosition+200,boxY,20,100,{isStatic:true})
+     World.add(world,boxThree);
+     
+
+    Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  box1.display();
+  box2.display();
+  box3.display();
+ 
+}
+
+
+// to drop the box
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    Body.setStatic(packageBody,false);
+    
+  }
+}
